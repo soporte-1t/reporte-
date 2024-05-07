d1f6e734715eda1b3d39ab0645f6450757d570e2
soporte-a1 <162223886+soporte-a1@users.noreply.github.com>
2024-05-06 21:17:31 -0600

    Update pass.js

--- js/pass.js
+++ js/pass.js
@@ -6,13 +6,13 @@ icon.addEventListener("click", e => {
     
          {
         usuario.type = "text"
-        contrasena.type = "text"
+        contraseña.type = "text"
         confirmar.type = "text";
         icon.classList.remove('bx-show-alt')
         icon.classList.add('bx-hide')
     } else {
         usuario.type = "password"
-        contrasena.type = "password"
+        contraseña.type = "password"
         confirmar.type = "password";
         icon.classList.add('bx-show-alt')
         icon.classList.remove('bx-hide')
